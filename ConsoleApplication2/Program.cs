7e986f7dee37f19fb8958f036186d96ec7a7492f
Illidan97 <32833116+Illidan97@users.noreply.github.com>
2017-10-16 09:56:15 +0200

    Add files via upload

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba dyrektor = new Osoba();
+            dyrektor.imie = "Hubert";
+            dyrektor.nazwisko = "Włoch";
+            dyrektor.rokUrodzenia = 1997;
+            dyrektor.waga = 90;
+            dyrektor.wzrost = 180;
+            dyrektor.okulary = true;
+            dyrektor.plec = "M";
+
+            Console.WriteLine(dyrektor.imie+ "\n" + dyrektor.nazwisko);
+            Console.WriteLine(dyrektor.ObliczWiek());
+            Console.WriteLine(dyrektor.Info());
+            Console.ReadKey();
+        }
+    }
+}
